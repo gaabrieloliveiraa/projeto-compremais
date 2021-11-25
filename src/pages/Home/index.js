d9c9e4c5d1915a5acfc528fe5328c23b91cb8c5e
Alexsandro Lima <alexsandrolima.dev@gmail.com>
2021-11-25 08:04:31 -0300

    envio dos dados para a api e refresh na home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,18 +1,39 @@
 import React, { useEffect, useState } from "react";
-import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import { RefreshControl, ActivityIndicator, FlatList, Text, View } from "react-native";
 import { Feather } from '@expo/vector-icons';
 
 import Header from '../../components/Header';
 import { Container, BannerItem, RateContainer, PromoList, SearchContainer, Input, SearchButton, Title, BannerButton, Banner, SliderOferta, Rate } from './styles';
+
 
 function Home() {
 
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
 
-  const getMovies = async () => {
+  /**
+   * Atualizando a tela utilizando o component RefreshControl
+   * Basta arrastar a tela para baixo, como fazemos nos apps geralmente, para atualizar a tela
+   */
+  const [refreshing, setRefreshing] = React.useState(false);
+
+  const onRefresh = React.useCallback(async () => {
+    setRefreshing(true);
     try {
-      const response = await fetch('http://10.0.2.2:3000/produtos')
+        
+        getProducts();
+        
+        setRefreshing(false)
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  , [refreshing]);
+
+  const getProducts = async () => {
+    try {
+      const response = await fetch('http://10.0.2.2:3000/produtos', {
+        method: 'GET'})
         .then((response) => response.json());
       setData(response);
     } catch (error) {
@@ -23,8 +44,10 @@ function Home() {
   }
 
   useEffect(() => {
-    getMovies();
+    getProducts();
   }, []);
+
+  
 
   return (
     <Container>
@@ -51,78 +74,13 @@ function Home() {
                 <Text>{item.preco}</Text>
               </>
             )}
+            refreshControl={
+              <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+            }
           />
         )}
       </View>
     </Container>
   );
 }
-
-//       return(
-//         <Container>
-//         <Header title="Página Inicial" />
-
-
-
-// {/* 
-//           <SearchContainer>
-//           <Input
-//           placeholder="Ex: Jogos para PS4"
-//           placeholderTextColor="#ddd"
-//               />
-
-//          <SearchButton>
-//             <Feather name="search" size={38} color="#FFF" />
-//             </SearchButton>
-//         </SearchContainer>
-
-//         <ScrollView>
-//           <Title>Em Destaque</Title>
-
-//           <BannerButton>
-//             <Banner
-//             resizeMethod="resize"
-//               source={{ uri: 'https://images.unsplash.com/photo-1633704797971-eacd062c9d8a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1173&q=80'}}
-//               />
-//           </BannerButton>
-
-//           <SliderOferta
-//             horizontal={true}
-//             showsHorizontalScrollIndicator={false}
-//             data={[1,2,3,4,5]}
-//             renderItem={ ({ item }) => <SliderItem /> }
-//             />
-
-//       <Title>Eletrodomésticos</Title>
-//       <SliderOferta   
-//             horizontal={true}
-//             showsHorizontalScrollIndicator={false}
-//             data={[1,2,3,4,5]}
-//             renderItem={({ item })=> <SliderItem/>}
-//             />
-
-//       <Title>SuperMercado</Title>
-//       <SliderOferta   
-//             horizontal={true}
-//             showsHorizontalScrollIndicator={false}
-//             data={[1,2,3,4,5]}
-//             renderItem={({ item })=> <SliderItem/>}
-//             />
-
-//       <Title>Games e Pc Gamer</Title>
-//       <SliderOferta   
-//             horizontal={true}
-//             showsHorizontalScrollIndicator={false}
-//             data={[1,2,3,4,5]}
-//             renderItem={({ item })=> <SliderItem/>}
-//             /> 
-
-//         </ScrollView>
-//  */}
-
-//       </Container>
-
-//      )
-//   }
-
 export default Home;
--- src/pages/Ofertas/index.js
+++ src/pages/Ofertas/index.js
@@ -1,13 +1,101 @@
 import React from "react";
-import { View, Text } from "react-native";
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
 
 
 function Ofertas(){
+  /**
+   * tela adaptada para efetuar o cadastro de produto
+   * basta clicar no botão para enviar o produto
+   * tem que colocar os campos nela e fazer a referência dos campos para enviar os dados
+   * 
+   */
+const getDataUsingPost = () => {
+
+  /**
+   * dados inseridos manualmente para envio
+   */
+  var dataToSend =  {
+    "nome": "Simbora",
+    "descricao": "Begoço baum da gota",
+    "preco": 10.0,
+    "categoria_id": 2
+  };  
+  
+  var formBody = [];
+  for (var key in dataToSend) {
+    var encodedKey = encodeURIComponent(key);
+    var encodedValue = encodeURIComponent(dataToSend[key]);
+    formBody.push(encodedKey + '=' + encodedValue);
+  }
+  formBody = formBody.join('&');
+  //POST request
+  fetch('http://10.0.2.2:3000/produtos', {
+    method: 'POST', //Request Type
+    body: formBody, //post body
+    headers: {
+      //Header Defination
+      'Content-Type': 
+        'application/x-www-form-urlencoded;charset=UTF-8',
+    },
+  })
+    .then((response) => response.json())
+    //If response is in json then in success
+    .then((responseJson) => {
+      alert(JSON.stringify(responseJson));
+      console.log(responseJson);
+    })
+    //If response is not in json then in error
+    .catch((error) => {
+      alert(JSON.stringify(error));
+      console.error(error);
+    });
+};
   return(
+    <SafeAreaView style={{flex: 1}}>
+      <View style={styles.container}>
+        <View style={styles.container}>
+          {/*Running POST Request*/}
+          <TouchableOpacity
+            style={styles.buttonStyle}
+            onPress={getDataUsingPost}>
+            <Text style={styles.textStyle}>
+              Mandando a bagaça
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
+      /*
       <View>
           <Text>TELA DE OFERTAS</Text>
       </View>
+      */
     )
   }
+
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: 'white',
+      justifyContent: 'center',
+      padding: 20,
+    },
+    textStyle: {
+      fontSize: 18,
+      color: 'white',
+    },
+    buttonStyle: {
+      alignItems: 'center',
+      backgroundColor: '#f4511e',
+      padding: 10,
+      marginVertical: 10,
+    },
+  });
 
 export default Ofertas;
